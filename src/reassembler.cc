b7425ae605cad1858a57d73ceaaa72cd8268d691
Yuxi Wang <1220798074@qq.com>
2024-10-11 10:14:44 +0800

    finish overlapping and 17 test

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,8 +4,10 @@ using namespace std;
 //  [----]      [-----------------] [ ---   ---   ---  ]  [............]
 //  poped       in the bytestream   in the reassembler   unacceptable
 #define first_unassembled_index output_.writer().bytes_pushed()
-// #define available_capacity output_.writer().available_capacity()
 #define first_unacceptable_index output_.writer().bytes_pushed()+output_.writer().available_capacity()
+#define bufStrEnd Reassemble_buf_[idx].first+Reassemble_buf_[idx].second.length()
+#define dataStrEnd dataFirst+data.length()
+
 
 void Reassembler::closeCheck()
 {
@@ -23,20 +25,38 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
   if(data.length()==0 || first_index + data.length()<first_unassembled_index || first_index>=first_unacceptable_index) {
     return;
   }
-  // 找到data插入Reassemble_buf_的index
+  // 除去capcity外的字符串
   data = data.substr(0,min(data.length(), first_unacceptable_index-first_index));
   pair<uint64_t, string> substring{first_index, data};
+  // 找到data插入Reassemble_buf_的index
   uint64_t idx = 0;
-  while(idx < Reassemble_buf_.size() && substring.first > Reassemble_buf_[idx].first)
-    idx++;
-  // 插入之后需要消除overlap
-  if(idx < Reassemble_buf_.size() && substring.first == Reassemble_buf_[idx].first){
-    if(substring.second.length() > Reassemble_buf_[idx].second.length()) {
-      Reassemble_buf_.erase( Reassemble_buf_.begin()+idx);
-      Reassemble_buf_.insert(Reassemble_buf_.begin()+idx, substring);
+  uint64_t dataFirst = first_index;
+  bool insertFlag = true;
+  while(idx < Reassemble_buf_.size()) {
+    if(dataFirst > bufStrEnd) // 6
+      idx++;
+    else if(dataStrEnd<Reassemble_buf_[idx].first) // 5
+      break;
+    else if(dataFirst < Reassemble_buf_[idx].first && dataStrEnd >= Reassemble_buf_[idx].first && dataStrEnd<bufStrEnd){ //1
+      data += Reassemble_buf_[idx].second.substr(dataStrEnd - Reassemble_buf_[idx].first);
+      Reassemble_buf_.erase(Reassemble_buf_.begin()+idx);
+    }else if(dataFirst > Reassemble_buf_[idx].first&& dataFirst <= bufStrEnd && dataStrEnd > bufStrEnd ) { // 2
+      data  = Reassemble_buf_[idx].second.substr( 0, dataFirst-Reassemble_buf_[idx].first) + data;
+      dataFirst = Reassemble_buf_[idx].first;
+      Reassemble_buf_.erase(Reassemble_buf_.begin()+idx);
+    }else if(dataFirst <=Reassemble_buf_[idx].first && dataStrEnd>= bufStrEnd) { // 3
+      Reassemble_buf_.erase(Reassemble_buf_.begin() + idx);
     }
-  }else
+    else { // 4
+      insertFlag = false;
+      break;
+    }
+  }
+  if(insertFlag) {
+    substring = pair<uint64_t, string>(dataFirst, data);
     Reassemble_buf_.insert(Reassemble_buf_.begin()+idx, substring);
+  }
+
   // 开始写入ByteStream
   while(!Reassemble_buf_.empty() && Reassemble_buf_[0].first<=first_unassembled_index) {
     if(Reassemble_buf_[0].first+Reassemble_buf_[0].second.length()>first_unassembled_index){
@@ -56,25 +76,31 @@ uint64_t Reassembler::bytes_pending() const
   return sum;
 }
 
-// 废弃code
-// if(first_index <= first_unassembled_index) {
-//   // 与bytestream完全重复:扔掉
-//   if(first_index + data.length() <= first_unassembled_index) {
-//     return;
-//   }// 与bytestream部分重复:取剩余
-//   uint64_t size = min(first_index+data.length()-first_unassembled_index,available_capacity );
-//   output_.writer().push( data.substr( 0, size ) );
-//
-//   pair<uint64_t, string> substring = Reassemble_buf_[0];
-//   while(substring.first + substring.second.length() > first_unassembled_index) {
-//     ;
+
+// 相同first_index 不同length()
+// if(idx < Reassemble_buf_.size() && substring.first == Reassemble_buf_[idx].first){
+//   if(substring.second.length() > Reassemble_buf_[idx].second.length()) {
+//     Reassemble_buf_.erase( Reassemble_buf_.begin()+idx);
+//     Reassemble_buf_.insert(Reassemble_buf_.begin()+idx, substring);
 //   }
-
-// 将有效片段放入Reassemble_buf_
-// if(first_index<first_unassembled_index && first_index+data.length()>first_unassembled_index) {
-//   uint64_t size = min(first_index+data.length()-first_unassembled_index,available_capacity );
-//   substring = pair<uint64_t, string>{first_unassembled_index, data.substr(first_unassembled_index-first_index, size)};
 // }else
-//   substring = pair<uint64_t, string>{first_index, data};
+//   Reassemble_buf_.insert(Reassemble_buf_.begin()+idx, substring);
 
 
+// while(idx < Reassemble_buf_.size() && substring.first > Reassemble_buf_[idx].first)
+//   idx++;
+// // 插入之后需要消除overlap
+// uint64_t substrEnd = substring.first+substring.second.length();
+// while(idx < Reassemble_buf_.size()) {
+//   // 1.totally overlap
+//   if(substrEnd >= Reassemble_buf_[idx].first+Reassemble_buf_[idx].second.length())
+//     Reassemble_buf_.erase( Reassemble_buf_.begin()+idx );
+//   // 2. partially overlap
+//   else if(substrEnd >= Reassemble_buf_[idx].first && substrEnd < Reassemble_buf_[idx].first+Reassemble_buf_[idx].second.length()) {
+//     substring.second += Reassemble_buf_[idx].second.substr( substrEnd-Reassemble_buf_[idx].first );
+//     Reassemble_buf_.erase( Reassemble_buf_.begin()+idx );
+//   }
+//   // 3. no overlap
+//   else
+//     break;
+// }
